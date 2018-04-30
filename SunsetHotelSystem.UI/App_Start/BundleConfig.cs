ffcabf11d9a72c57ea204703fcbc392b9df3103a
kevinvarp18 <31193307+kevinvarp18@users.noreply.github.com>
2018-04-30 11:40:51 -0600

    asdfghj

--- SunsetHotelSystem.UI/App_Start/BundleConfig.cs
+++ SunsetHotelSystem.UI/App_Start/BundleConfig.cs
@@ -6,20 +6,25 @@ namespace SunsetHotelSystem.UI {
 
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.flexisel.js",
                         "~/Scripts/jquery-2.1.4.min.js",
-                        "~/Scripts/main.js",
+                        "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/javascript").Include(
                         "~/Scripts/move-top.js",
-                        "~/Script/easing.js",
-                        "~/Script/jquery.typer.js",
-                        "~/Script/easy-reponsive-tabs.js"));
+                        "~/Scripts/easing.js",
+                        "~/Scripts/jquery.typer.js",
+                        "~/Scripts/easy-responsive-tabs.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/font-awesome.css",
                       "~/Content/easy-responsive-tabs.css",
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/style.css"));
         }
     }//Fin de la clase BundleConfig.
