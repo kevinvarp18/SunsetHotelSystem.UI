a8a1351be3b73d88f7e08496cae996b2f81d77d8
kevinvarp18 <31193307+kevinvarp18@users.noreply.github.com>
2018-04-30 01:50:56 -0600

    incluir template en la solución

--- SunsetHotelSystem.UI/App_Start/BundleConfig.cs
+++ SunsetHotelSystem.UI/App_Start/BundleConfig.cs
@@ -1,31 +1,26 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace SunsetHotelSystem.UI
-{
-    public class BundleConfig
-    {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
+namespace SunsetHotelSystem.UI {
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/main.js",
+                        "~/Scripts/jquery-2.1.4.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/move-top.js",
+                      "~/Scripts/easing.js",
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/jquery.typer.js",
+                      "~/Scripts/easy-responsive-tabs.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome.css",
+                      "~/Content/easy-responsive-tabs.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/style.css"));
         }
-    }
-}
+    }//Fin de la clase BundleConfig.
+}//Fin del namespace.
